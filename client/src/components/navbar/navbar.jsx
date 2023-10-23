2057e628435cdf68c8554abc4e3969236e7ff697
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-10-23 15:03:05 +0300

    add serach and clear

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 
-import {Box, Stack} from '@mui/material'
+import {Box, Stack, TextField,Button} from '@mui/material'
 import { useEffect ,useState} from 'react'
 import img1 from '../../assets/nav.gif'
 import img3 from '../../assets/nav3.png'
@@ -9,11 +9,19 @@ import LiquorIcon from '@mui/icons-material/Liquor';
 import PaymentsIcon from '@mui/icons-material/Payments';
 import StarsIcon from '@mui/icons-material/Stars';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import IconButton from '@mui/material/IconButton';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 import {Link} from 'react-router-dom'
 
 const Navbar=()=>{
     const [count,setCount]=useState(1)
+    const [search,setSearch]=useState('')
+    
     useEffect(()=>{
         const interval=setInterval(()=>{
                 setCount(count+1)
@@ -46,13 +54,22 @@ const Navbar=()=>{
                  <Link to='/' style={{ textDecoration:'none' }}  >
                   <h1 style={{color:'black',marginLeft:40,fontSize:34,fontFamily:'fantasy'}}>HuleBebere <ShoppingCartIcon sx={{color:'#a84119',fontSize:40}}/></h1> 
                  </Link>
-                 <Box sx={{ border:'1px solid',width:450,height:40 }}>
+                 <Stack direction='row' pl={1} mt={3} sx={{ border:'1px solid',width:430,height:31,borderRadius:1 }}>
+                 <SearchIcon sx={{paddingTop:0.5}}/>
                    <input 
-                   type='text'
-                   placeholder='search everything'
-                   style={{ border:'none' }}
+                     type='text'
+                     placeholder='Search products,brands and catagories'
+                     onChange={(e)=>setSearch(e.target.value)}
+                      style={{ border:'none',outline:'none',width:370 }}
+                      value={search}
+
                     />
-                 </Box>
+                    {search && <IconButton onClick={()=>setSearch('')}><ClearOutlinedIcon  sx={{paddingTop:0.3}}/></IconButton> }
+                 </Stack>
+                 <Stack direction='row' spacing={1}>
+                   <Button variant='contained' sx={{ height:30,backgroundColor:'#a84119',width:78 }}>Search</Button>
+                    <Button> <ManageAccountsOutlinedIcon /> Account<></> </Button>
+                 </Stack>
                 </Stack>
         
         
