ed4ca1d3f90c071e4306ef0240ae8d8f4a836c98
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-11-01 20:43:46 +0300

    chnage the navigation way of working

--- client/src/components/bodyOne/bodyOne_two.jsx
+++ client/src/components/bodyOne/bodyOne_two.jsx
@@ -5,17 +5,46 @@ import IconButton from '@mui/material/IconButton';
 import { useEffect, useState } from "react";
 
 import img1 from '../../assets/bodyone/img1.jpg'
-import img2 from  '../../assets/bodyone/img8.jpg'
-import img3 from  '../../assets/bodyone/img2.jpg'
-import img4 from  '../../assets/bodyone/img15.jpg'
-import img5 from  '../../assets/bodyone/img14.jpg'
-import img6 from  '../../assets/bodyone/img18.jpg'
-import img7 from  '../../assets/bodyone/img24.jpg'
+import img11 from '../../assets/bodyone/img3.jpg'
+import img111 from '../../assets/bodyone/img12.jpg'
+import img1111 from '../../assets/bodyone/img9.jpg'
+
+
+import img2 from  '../../assets/bodyone/img27.jpg'
+
+import img3 from  '../../assets/bodyone/img15.jpg'
+import img33 from  '../../assets/bodyone/img10.jpg'
+import img333 from  '../../assets/bodyone/img11.jpg'
+import img3333 from  '../../assets/bodyone/img20.jpg'
+
+import img4 from  '../../assets/bodyone/img26.jpg'
+
+import img5 from  '../../assets/bodyone/img2.jpg'
+import img55 from  '../../assets/bodyone/img16.jpg'
+import img555 from  '../../assets/bodyone/img19.jpg'
+import img5555 from  '../../assets/bodyone/img23.jpg'
+
+
+import img6 from  '../../assets/bodyone/img27.jpg'
+
+import img7 from  '../../assets/bodyone/img4.jpg'
+import img77 from  '../../assets/bodyone/img5.jpg'
+import img777 from  '../../assets/bodyone/img24.jpg'
+import img7777 from  '../../assets/bodyone/img8.jpg'
+
 import img8 from  '../../assets/bodyone/img25.jpg'
-import img9 from  '../../assets/bodyone/img26.jpg'
-import img10 from  '../../assets/bodyone/img27.jpg'
 
+ import img9 from  '../../assets/bodyone/img30.jpg'
+ import img99 from  '../../assets/bodyone/32.jpg'
+ import img999 from  '../../assets/bodyone/33.jpg'
+ import img9999 from  '../../assets/bodyone/34.jpg'
 
+import img10 from  '../../assets/bodyone/img28.jpg'
+import img100 from  '../../assets/bodyone/31.jpg'
+import img1000 from  '../../assets/bodyone/35.jpg'
+import img10000 from  '../../assets/bodyone/img18.jpg'
+
+import style from './BO.css'
 
 const BodyOneTwo=()=>{
     const [count,setCount]=useState(1)
@@ -23,27 +52,64 @@ const BodyOneTwo=()=>{
     useEffect(()=>{
       const interval=setInterval(()=>{
         setCount((prev)=>prev+1)
-        if(count===11){setCount(1)}
+        if(count===40){setCount(1)}
         
-       },2000)
+       },1000)
         return ()=> clearInterval(interval)
      },[count])
-    
+
+  
     return(
     <Box> 
-        <img  src={img1} style={{ height:423,width:620 }}/>
+      {(count===1)&&<img  src={img1} style={{ height:423,width:620 }}/>}
+      {(count===2)&&<img  src={img11} style={{ height:423,width:620 }}/>}
+      {(count===3)&&<img  src={img111} style={{ height:423,width:620 }}/>}
+      {(count===4)&&<img  src={img1111} style={{ height:423,width:620 }}/>}
+      
+      {(count===5 ||  count===6 ||  count=== 7 || count===8 )&&<img  src={img2} style={{ height:423,width:620 }}/>}
+
+      {(count===9)&&<img  src={img3} style={{ height:423,width:620 }}/>}
+      {(count===10)&&<img  src={img33} style={{ height:423,width:620 }}/>}
+      {(count===11)&&<img  src={img333} style={{ height:423,width:620 }}/>}
+      {(count===12)&&<img  src={img3333} style={{ height:423,width:620 }}/>}
+      
+      {(count===13 ||  count===14 ||  count=== 15 || count===16 )&&<img  src={img4} style={{ height:423,width:620 }}/>}
+
+      {(count===17)&&<img  src={img5} style={{ height:423,width:620 }}/>}
+      {(count===18)&&<img  src={img55} style={{ height:423,width:620 }}/>}
+      {(count===19)&&<img  src={img555} style={{ height:423,width:620 }}/>}
+      {(count===20)&&<img  src={img5555} style={{ height:423,width:620 }}/>}
+      
+      {(count===21 ||  count===22 ||  count=== 23 || count===24 )&&<img  src={img6} style={{ height:423,width:620 }}/>}
+
+      {(count===25)&&<img  src={img7} style={{ height:423,width:620 }}/>}
+      {(count===26)&&<img  src={img77} style={{ height:423,width:620 }}/>}
+      {(count===27)&&<img  src={img777} style={{ height:423,width:620 }}/>}
+      {(count===28)&&<img  src={img7777} style={{ height:423,width:620 }}/>}
+      
+      {(count===29 ||  count===30 ||  count===31 || count===32 )&&<img  src={img8} style={{ height:423,width:620 }}/>}
+      
+      {(count===33)&&<img  src={img9} style={{ height:423,width:620 }}/>}
+      {(count===34)&&<img  src={img99} style={{ height:423,width:620 }}/>}
+      {(count===35)&&<img  src={img999} style={{ height:423,width:620 }}/>}
+      {(count===36)&&<img  src={img9999} style={{ height:423,width:620 }}/>}
+
+      {(count===37)&&<img  src={img10} style={{ height:423,width:620 }}/>}
+      {(count===38)&&<img  src={img100} style={{ height:423,width:620 }}/>}
+      {(count===39)&&<img  src={img1000} style={{ height:423,width:620 }}/>}
+      {(count===40)&&<img  src={img10000} style={{ height:423,width:620 }}/>}
 
         <Stack direction='row' spacing={1}>
-         <IconButton onClick={()=>{setCount(1)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===1 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(2)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===2 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(3)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===3 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(4)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===4 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(5)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===5 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(6)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===6 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(7)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===7 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(8)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===8 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(9)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===9 &&{color:'#86442d'})}}/></IconButton>
-         <IconButton onClick={()=>{setCount(10)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...(count===10 &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(1)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===1 || count===2 || count===3 || count===4) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(5)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===5 || count===6 || count===7 || count===8)&&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(9)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===9 || count===10 || count===11 || count===12)&&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(13)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===13 || count===14 || count===15 || count===16) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(17)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===17 || count===18 || count===19 || count===20) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(21)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===21 || count===22 || count===23 || count===24) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(25)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===25 || count===26 || count===27 || count===28)&&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(29)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===29 || count===30 || count===31 || count===32) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(33)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===33 || count===34 || count===35 || count===36) &&{color:'#86442d'})}}/></IconButton>
+         <IconButton onClick={()=>{setCount(37)}} sx={{ width:10,height:10}}><Brightness1Icon  sx={{fontSize:12, ...((count===37 || count===38 || count===39 || count===40) &&{color:'#86442d'})}}/></IconButton>
 
         
         
